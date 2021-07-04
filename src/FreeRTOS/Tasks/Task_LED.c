898587747b69267a1e418f31562e24eccf6d94f3
qitas <qitas@qitas.cn>
2021-07-04 14:21:19 +0800

    fine

--- src/FreeRTOS/Tasks/Task_LED.c
+++ src/FreeRTOS/Tasks/Task_LED.c
@@ -5,12 +5,14 @@ TaskHandle_t H_Task_LED;
 #define PRIORITY_TASKLED	(tskIDLE_PRIORITY+3)
 static void Task_LED(void* param)
 {
-	User_Gpio_Init();	
+	User_Gpio_Init();
 	while(1)
-		{
-//			LED0_Toggle();
-		    vTaskDelay(1000/portTICK_PERIOD_MS);
-		}
+	{
+		// LED0_Toggle();
+		PORT_Toggle(LED2_PORT,LED2_Pin);
+		PORT_Toggle(LED1_PORT,LED1_Pin);
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
 }
 uint8_t Task_LED_Start(void)
 {
